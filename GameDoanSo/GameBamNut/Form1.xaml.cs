ae60c391e93a98a87bb2e60577c5e0f4894755a8
thanhnthe186221 <tienthanh22092004@gmail.com>
2026-01-28 13:25:59 +0700

    Add project
    Add GameDoanSo C# project

---
+++ GameDoanSo/GameBamNut/Form1.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace GameBamNut
+{
+    // Phải có chữ "partial class" và ": Form"
+    public partial class Form1 : Form
+    {
+        int diemSo = 0;
+
+        public Form1()
+        {
+            InitializeComponent(); // Dòng này nếu bị đỏ là do bạn đang ở sai file
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Code xử lý nút bấm ở đây
+            diemSo++;
+            label1.Text = "Điểm: " + diemSo; // label1 phải tồn tại bên giao diện trước
+        }
+    }
+}
