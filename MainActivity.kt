340f627ff392da4d1e66e8227bf2efc58572b2aa
BaoHGHE153305 <baohghe153305@gmail.com>
2026-01-28 21:08:35 +0700

    Update and rename ios project to MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,17 @@
+// Empty project init
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.btnHello)
+        btn.setOnClickListener {
+            Toast.makeText(this, "xin chào", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
